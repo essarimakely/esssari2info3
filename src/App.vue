131ccaf92ec9b9481c264e117855348960f47f13
Essari <makely132@gmail.com>
2022-06-06 11:15:11 -0300

    aula

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import HeaderComp from "@/components/template/HeaderComp.vue";
+import RodapeComp from "@/components/template/RodapeComp.vue";
+</script>
+
+<template>
+  <HeaderComp></HeaderComp>
+  <RouterView />
+  <RodapeComp></RodapeComp>
+</template>
+<style>
+@import "@/assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  height: 100vh;
+
+  font-weight: normal;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+footer {
+  height: 15%;
+  background-color: rgb(57, 57, 128);
+  color: white;
+  font-size: 1.3rem;
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
+  justify-content: space-between;
+  padding-left: 20px;
+}
+header span {
+  padding: 0 20px;
+  padding-right: 20px;
+}
+main {
+  height: 80%;
+}
+</style>
---
+++ src/views/TimesView.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="container">
+    <div class="title">
+      <h2>Gerenciamento de times</h2>
+    </div>
+    <div class="form-input">
+      <input type="text" />
+      <buttton>Salvar</buttton>
+    </div>
+    <div class="list-times">
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Time 1</td>
+            <td>??????</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Time 2</td>
+            <td>??????</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Time 3</td>
+            <td>??????</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+<style>
+.title {
+  text-align: center;
+  margin: 2rem 0;
+}
+
+.form-input {
+  display: flex;
+  justify-content: center;
+  margin: 2rem 0;
+}
+
+.form-input input {
+  width: 50%;
+  padding: 0.5rem;
+  border: 1px solid;
+  border-radius: 10px;
+}
+.form-input button {
+  padding: 0.5rem;
+  width: 15%;
+  padding: 0.5rem;
+  border: 1px solid rgb(red);
+  border-radius: 10px;
+  color: white;
+  font-weight: bold;
+  margin-left: 1%;
+}
+
+.list.times {
+  display: flex;
+  justify-content: center;
+}
+
+table {
+  width: 50%;
+  border-collapse: collapse;
+  margin: 0 auto;
+  border: 1px solid black;
+  font-size: 1.1rem;
+}
+table thead {
+  background-color: black;
+  color: white;
+  font-weight: bold;
+}
+table thead th {
+  font-weight: bolder;
+}
+
+table tbody tr {
+  text-align: center;
+}
+table tbody tr:nth-child(odd) {
+  background-color: rgb(red, green, blue);
+}
+</style>
